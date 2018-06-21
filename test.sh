2d53c3a0c757c4423646e64b4cb3ab968a5c699d
Test User <eng-serv+bbstestuser@hashicorp.com>
2018-06-21 15:28:33 -0400

    fixed code

--- test.sh
+++ test.sh
@@ -4,18 +4,17 @@
 
 # Extract code argument from the query map
 eval "$(jq -r '@sh "export CODE=\(.code)"')"
-#echo "CODE is: $CODE"
 
 case $CODE in
      1)
-        BALANCE=100
+        export BALANCE=100
         ;;
      2)
-        BALANCE=0
+        export BALANCE=0
         ;;
      *)
-        BALANCE=0
+        export BALANCE=0
         ;;
 esac
 
-jq -n --arg balance "$BALANCE" '{"balance":$balance}'
+jq -n --arg the_balance "$BALANCE" '{"balance":$the_balance}'
